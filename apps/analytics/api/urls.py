564bff99f5df2448f9c9b640c65270f3096f5a8a
Apatira Sulayman <devspark015@gmail.com>
2026-05-23 21:46:10 +0100

    Revert "Feature/backend setup"

--- apps/analytics/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from apps.analytics.api.views import SessionAnalyticsView
-
-
-urlpatterns = [
-	path("sessions/<int:session_id>/", SessionAnalyticsView.as_view(), name="session-analytics"),
-]
--- apps/responses/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResponsesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.responses"
-    verbose_name = "Student Responses"
